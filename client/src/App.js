6c9da42c964d0706ad6e960b7c75a0b4092d06f9
Karsan Uthayakumar <kum1t3p1@gamil.com>
2020-10-08 16:49:00 -0400

    Initial commit react

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/navbar'
+import './App.css';
+
+function App() {
+  return (
+    // navbar
+    <Navbar/>  
+    // conditional rendering 
+    
+  );
+}
+
+export default App;
