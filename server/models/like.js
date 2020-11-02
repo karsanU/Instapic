e5b75fd2ce36fc3f3bb01fb3d63c4adb75b58ef8
Karsan Uthayakumar <kum1t3p1@gamil.com>
2020-11-02 18:46:34 -0500

    server init

---
+++ server/models/like.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const likeSchema = Schema({
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Post",
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  time: { type: Date, default: Date.now },
+});
