5721ad013151a5547e09e55f09635639b94e527f
Karsan Uthayakumar <kum1t3p1@gamil.com>
2020-11-17 05:17:16 -0500

    server modification for login and signup endpoint

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,72 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
 
 const userSchema = Schema({
-  name: String,
-  email: String,
-  password: String,
-  userName: String,
-  time: { type: Date, default: Date.now },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  userName: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  time: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
 });
 
-const Task = mongoose.model("User", userSchema);
-module.exports = Task;
+// Methods are only accessible on the instances of the model.
+userSchema.methods.generateAuthToken = async function (req, res) {
+  const user = this;
+  const token = jwt.sign(
+    { _id: user._id.toString() },
+    "this is a secret key....."
+  );
+  return token;
+};
+
+// Check user's login credentials.
+// Statics are accessible on the models.
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error("Unable to login.");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error("Unable to login.");
+  }
+
+  return user;
+};
+
+// Hash the passwords upon creation and update.
+userSchema.pre("save", async function (next) {
+  // The current user that is about to be saved.
+  const user = this;
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  // Next must be called to finish the process of saving the user.
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const multer = require("multer");
+const sharp = require("sharp");
 const User = require("../models/user");
+const auth = require("../middleware/auth");
 
 const router = new express.Router();
 
@@ -9,12 +12,55 @@ router.post("/users/create", async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
-    res.status(201).send({ user });
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
   } catch (e) {
     res.status(400).send(e);
   }
 });
 
+// see if a given email already exists
+router.get("/users/isEmailUnique/:email", async (req, res) => {
+  email = req.params.email;
+  try {
+    if ((await User.find({ email }).exec()).length < 1) {
+      res.status(200).send({ emailExists: false });
+    } else {
+      res.status(200).send({ emailExists: true });
+    }
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+// see if a given username already exists
+router.get("/users/isUsernameUnique/:username", async (req, res) => {
+  userName = req.params.username;
+  try {
+    console.log(req);
+    if ((await User.find({ userName }).exec()).length < 1) {
+      res.status(200).send({ usernameExists: false });
+    } else {
+      res.status(200).send({ usernameExists: true });
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(400).send(e);
+  }
+});
+
+//login a user
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    )
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
+  } catch (e) {
+    console.log(e);
+    res.status(400).send(e);
+  }
+});
 module.exports = router;
-
-
