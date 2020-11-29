40879121162097e1963a7e84a47cefe7a88daf2c
Karsan Uthayakumar <kum1t3p1@gmail.com>
2020-11-29 17:55:21 -0500

    post deletion added

--- server/routes/post.js
+++ server/routes/post.js
@@ -43,6 +43,39 @@ router.post(
   }
 );
 
+// remove a post
+router.post(
+  "/posts/delete",
+  auth,
+  upload.single("image"),
+  async (req, res) => {
+    // remove all the comments 
+    console.log(req.body)
+    req.body.comments.forEach((comment) => {
+      Comment.findByIdAndDelete(comment._id, function (err) {
+        if (err) console.log(err);
+        console.log("Successful deletion of comment");
+      });
+    })
+    // remove the post from user 
+    req.user.posts = req.user.posts.filter((post) => {
+      post !== post._id
+    })
+    await req.user.save()
+    // remove the post
+    Post.findByIdAndDelete(req.body._id, function (err) {
+      if (err) console.log(err);
+      console.log("Successful deletion of post");
+    })
+    res.status(200).send();
+  },
+  (e, res) => {
+    console.log(e);
+    res.status(400).send({ error: e.message });
+  }
+);
+
+
 // get the img src of a file given the post _id
 router.get("/posts/picture/:id", auth, async (req, res) => {
   try {
@@ -127,6 +160,7 @@ router.post("/comments/delete", auth, async (req, res) => {
       return String(comment._id) !== String(req.body.comment._id)
     })
     await post.save()
+
     // remove the comment document 
     Comment.findByIdAndDelete(req.body.comment._id, function (err) {
       if (err) console.log(err);
